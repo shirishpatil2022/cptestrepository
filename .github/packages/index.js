8f1f67e9580a18c51a4cbfd3efbede61ca8b6063
shirishpatil2022 <shirish_patil@colpal.com>
2022-11-02 10:23:24 +0530

    third commit

---
+++ .github/packages/index.js
@@ -0,0 +1,12 @@
+const core= require('@actions/core');
+const github=require('@actions/github')
+try{
+    const name=core.getInput('who-to-greet');
+    console.log(`Hello ${name}`);
+    const time=(new Date()).toTimeString();
+    core.setOutput("time",time);
+    const payload=JSON.stringify(github.context.payload,undefined,2);
+    console.log(`the event payload: ${payload}`);
+}catch(err){
+    core.setFailed(err.message);
+}
